7cbc585eb72d757d794ac7cba04f27f1e9f21640
PltOka <PltOka@PltPlt.Plt.com>
2024-11-27 16:45:38 +0900

    prj 2.7 検証コントロールのサンプルプログラム
    Valid.aspx
    テキストボックスの中身をValidate!

--- SelfAspNet/SelfAspNet/Global.asax.cs
+++ SelfAspNet/SelfAspNet/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
+using System.Web.UI;
 
 namespace SelfAspNet
 {
@@ -11,6 +12,14 @@ namespace SelfAspNet
     {
         protected void Application_Start(object sender, EventArgs e)
         {
+            ScriptManager.ScriptResourceMapping.AddDefinition("jquery", null,
+                new ScriptResourceDefinition()
+                {
+                    Path = "~/Scripts/jquery-3.7.1.min.js",
+                    DebugPath = "~/Scripts/jquery-3.7.1.js",
+                    CdnPath = "http://ajax.microsoft.com/ajax/jQuery/ajax/jquery-3.7.1.min.js",
+                    CdnDebugPath = "http://ajax.microsoft.com/ajax/jQuery/ajax/jquery-3.7.1.js"
+                });
         }
     }
 }
