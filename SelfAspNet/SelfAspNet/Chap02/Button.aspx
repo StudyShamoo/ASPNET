6da3541c248c2b0e40b13ae2658f46d68928ca45
PltOka <PltOka@PltPlt.Plt.com>
2024-11-27 14:26:36 +0900

    prj 2.6 ボタンコントロールのサンプル
    Button, ImageButton, LinkButton それぞれのコントロールの
    OnClientClickプロパティにJavaScriptのconfirmメソッドを定義。

---
+++ SelfAspNet/SelfAspNet/Chap02/Button.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Button.aspx.cs" Inherits="SelfAspNet.Chap02.Button" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <script>
+        function ImgBtnConf() {
+            return confirm("イメージボタンだよん");
+        }
+        function LiBtnConf() {
+            return confirm("リンクボタンだよん");
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:Button ID="Button1" runat="server" Text="普通のボタンコントロールだよ" OnClick="Button1_Click" OnClientClick="return confirm(&quot;Buttonコントロールが動いたよん&quot;);" />
+            <br />
+            <asp:ImageButton ID="ImageButton1" runat="server" ImageUrl="~/Chap02/neko.png" OnClientClick="return ImgBtnConf();" />
+            <br />
+            <asp:LinkButton ID="LinkButton1" runat="server" OnClientClick="return LiBtnConf();">LinkButtonってやつだよ</asp:LinkButton>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ SelfAspNet/SelfAspNet/Chap02/Button.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <自動生成>
+//     このコードはツールによって生成されました。
+//
+//     このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+//     コードが再生成されるときに損失したりします。 
+// </自動生成>
+//------------------------------------------------------------------------------
+
+namespace SelfAspNet.Chap02
+{
+
+
+    public partial class Button
+    {
+
+        /// <summary>
+        /// form1 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Button1 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// ImageButton1 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton1;
+
+        /// <summary>
+        /// LinkButton1 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
+    }
+}
